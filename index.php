a3cfe67c619e7033aca38136d10febf36c4b2dc7
Sai-Varnitha-Reddy <68200915+Sai-Varnitha-Reddy@users.noreply.github.com>
2021-08-07 01:31:51 +0530

    c3

--- index.php
+++ index.php
@@ -1,35 +1,32 @@
-i!DOCTYPE html>
+<!DOCTYPE html>
 <html lang="en">
 
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="style.css">
+  <link rel="stylesheet" href="css/home.css">
   <title>Connect</title>
 </head>
 
-<body>
+<body class="homebody" style="background-color:white">
   <!-- Header -->
-  <section id="header">
-    <div class="header container">
-      <div class="nav-bar">
-        <div class="brand">
-          <a href="php/login.php">
-            <h1><span>S</span>haif <span>A</span>rfan</h1>
-          </a>
-        </div>
-        <div class="nav-list">
-          <div class="hamburger">
-            <div class="bar"></div>
+  <header>
+  <?php include('php/navbar.php'); ?>
+</header>
+<div class="c">
+        <div class="row">
+          <div class="col-md-6 col-sm-12" style="background-color: rgb(63, 40, 96); height:520px ;left:0px;">
+                  <div class="left" style="background-color: rgb(63, 40, 96); color:white; margin: 100px 50px;">
+                  <h1 style="text-align:center">Connect with friends and the world around you!</h1><br><br>
+                  <h5 style="text-align:center">Check out what's new on your timeline.</h5><br><center>
+                  <button type="button" class="btn btn-primary btn-lg" id="Button123"style="background-color: rgb(250, 139, 96);border-color: rgb(250, 139, 96);" onclick="window.location.href='register.php'" >Explore now</button>
+</center>  
+                </div>
           </div>
-          <ul>
-            <li><a href="#hero" data-after="Home">Home</a></li>
-            <li><a href="#services" data-after="Service">Services</a></li>
-            <li><a href="#projects" data-after="Projects">Projects</a></li>
-            <li><a href="#about" data-after="About">About</a></li>
-            <li><a href="#contact" data-after="Contact">Contact</a></li>
-          </ul>
+          <div class="col-md-6 col-sm-12">
+          <img class="card-img-top" style="right:-100px;width:380px; height:400px;"
+              src="img.jpg"
+              alt="img not available">         
+          </div>
         </div>
-      </div>
-    </div>
-  </section>
+  </div>
