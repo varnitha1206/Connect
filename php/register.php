16a8788d4451c07bdf1f245af05f72a78738011c
Sai-Varnitha-Reddy <68200915+Sai-Varnitha-Reddy@users.noreply.github.com>
2021-08-08 12:12:21 +0530

    c13

--- php/register.php
+++ php/register.php
@@ -4,6 +4,10 @@
     <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">  
     <link rel = "stylesheet" type = "text/css" href = "../css/home.css"> 
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Dancing+Script:wght@700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <script>
  
  function validation()  
@@ -49,7 +53,24 @@
 
 <body style=" background-color:rgb(63, 40, 96);">
 <header>
-      <?php include('navbar.php'); ?>
+<nav class="navbar navbar-expand-lg" style="background-color:rgb(95, 218, 152);">
+                <a class="navbar-brand" href="../index.php" style="color: white;">
+                    <strong><h2>Connect</h2></strong> 
+                  </a>
+            
+              <div style="position:absolute;right:30px;">
+                   <?php
+                   session_start();
+                   if (isset($_SESSION['email'])) {
+                    echo '<a href="feed.php" class="btn btn-warning" id="b12" style="background-color: rgb(250, 139, 96); border:solid rgb(250, 139, 96) 2px;margin-left:4px; color:white;">Hi, '.$_SESSION['uname'].'</a> 
+                     <a href="logout.php" class="btn btn-danger" id="b13" style="background-color: rgb(250, 139, 96);border:solid rgb(250, 139, 96) 2px;">Logout</a>';
+                    } else {
+                    echo '<a href="login.php" class="btn btn-success id="b14"" data-target="modal1" style="background-color: rgb(250, 139, 96); border:solid rgb(250, 139, 96) 2px;">Login</a>&nbsp
+                    <a href="register.php" class="btn btn-success" id="b15" data-target="modal2" style="background-color: rgb(250, 139, 96); border:solid rgb(250, 139, 96) 2px;">Register</a>';
+                    }
+                    ?>
+                    
+                </div>
 </header> 
     <div id = "frm" style="width:365px;">   
         <img src="https://www.pngkit.com/png/detail/128-1280585_user-icon-fa-fa-user-circle.png" width="100px" height="100px"></center>
